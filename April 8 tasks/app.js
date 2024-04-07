733444fffe22d9a888347c334b8fba3da84ba628
ElminNabiyev <bd6wljfbp@code.edu.az>
2024-04-07 23:47:42 +0400

    Some changes added

--- April 8 tasks/app.js
+++ April 8 tasks/app.js
@@ -6,23 +6,22 @@ const userImg = document.createElement("img");
 const compImg = document.createElement("img");
 
 buttons[0].addEventListener("click", function () {
-  createImgas();
+  createImegas();
   userImg.src =
-    "https://file.removal.ai/preview/b9a522da-2292-429c-b216-79668865911a-dwayne-the-rock-johnson-gettyimages-1061959920.png";
+    "https://www.slazzer.com/downloads/b997ce66-f516-11ee-9f1b-42010a80000a/%5Bremoval.ai%5D_b9a522da-2292-429c-b216-79668865911a-dwayne-the-rock-johnson-gettyimages-1061959920_prev_ui.png";
   createCompChoice();
   getResult();
 });
 buttons[1].addEventListener("click", function () {
-  createImgas();
+  createImegas();
   userImg.src =
-    "https://file.removal.ai/preview/a14a167d-1688-4040-b989-96a42927a718-20568308-rock-paper-scissors-paper-hand-isolated-on-white.png";
+    "https://www.slazzer.com/downloads/20e72134-f517-11ee-8c36-42010a80000a/1000_F_243068964_BqrpjOdMd2U7ZEGLjiO8l7h4Ut1j9hcA_prev_ui.png";
   createCompChoice();
   getResult();
 });
 buttons[2].addEventListener("click", function () {
-  createImgas();
-  userImg.src =
-    "https://files.modern.az/articles/2021/09/29/467399.jpg";
+  createImegas();
+  userImg.src = "https://files.modern.az/articles/2021/09/29/467399.jpg";
   createCompChoice();
   getResult();
 });
@@ -31,15 +30,14 @@ function createCompChoice() {
   const random = Math.floor(Math.random() * 4);
   if (random === 1) {
     compImg.src =
-      "https://file.removal.ai/preview/b9a522da-2292-429c-b216-79668865911a-dwayne-the-rock-johnson-gettyimages-1061959920.png";
+      "https://www.slazzer.com/downloads/b997ce66-f516-11ee-9f1b-42010a80000a/%5Bremoval.ai%5D_b9a522da-2292-429c-b216-79668865911a-dwayne-the-rock-johnson-gettyimages-1061959920_prev_ui.png";
   }
   if (random === 2) {
     compImg.src =
-      "https://file.removal.ai/preview/a14a167d-1688-4040-b989-96a42927a718-20568308-rock-paper-scissors-paper-hand-isolated-on-white.png";
+      "https://www.slazzer.com/downloads/20e72134-f517-11ee-8c36-42010a80000a/1000_F_243068964_BqrpjOdMd2U7ZEGLjiO8l7h4Ut1j9hcA_prev_ui.png";
   }
   if (random === 3) {
-    compImg.src =
-      "https://files.modern.az/articles/2021/09/29/467399.jpg";
+    compImg.src = "https://files.modern.az/articles/2021/09/29/467399.jpg";
   }
 }
 function getResult() {
@@ -48,55 +46,51 @@ function getResult() {
   }
   if (
     compImg.src ===
-      "https://file.removal.ai/preview/b9a522da-2292-429c-b216-79668865911a-dwayne-the-rock-johnson-gettyimages-1061959920.png" &&
+      "https://www.slazzer.com/downloads/b997ce66-f516-11ee-9f1b-42010a80000a/%5Bremoval.ai%5D_b9a522da-2292-429c-b216-79668865911a-dwayne-the-rock-johnson-gettyimages-1061959920_prev_ui.png" &&
     userImg.src ===
-      "https://file.removal.ai/preview/a14a167d-1688-4040-b989-96a42927a718-20568308-rock-paper-scissors-paper-hand-isolated-on-white.png"
+      "https://www.slazzer.com/downloads/20e72134-f517-11ee-8c36-42010a80000a/1000_F_243068964_BqrpjOdMd2U7ZEGLjiO8l7h4Ut1j9hcA_prev_ui.png"
   ) {
     result.textContent = "Uddun";
   }
   if (
     compImg.src ===
-      "https://file.removal.ai/preview/b9a522da-2292-429c-b216-79668865911a-dwayne-the-rock-johnson-gettyimages-1061959920.png" &&
-    userImg.src ===
-      "https://files.modern.az/articles/2021/09/29/467399.jpg"
+      "https://www.slazzer.com/downloads/b997ce66-f516-11ee-9f1b-42010a80000a/%5Bremoval.ai%5D_b9a522da-2292-429c-b216-79668865911a-dwayne-the-rock-johnson-gettyimages-1061959920_prev_ui.png" &&
+    userImg.src === "https://files.modern.az/articles/2021/09/29/467399.jpg"
   ) {
     result.textContent = "Uduzdun";
   }
   if (
-    compImg.src ===
-      "https://files.modern.az/articles/2021/09/29/467399.jpg" &&
+    compImg.src === "https://files.modern.az/articles/2021/09/29/467399.jpg" &&
     userImg.src ===
-      "https://file.removal.ai/preview/a14a167d-1688-4040-b989-96a42927a718-20568308-rock-paper-scissors-paper-hand-isolated-on-white.png"
+      "https://www.slazzer.com/downloads/20e72134-f517-11ee-8c36-42010a80000a/1000_F_243068964_BqrpjOdMd2U7ZEGLjiO8l7h4Ut1j9hcA_prev_ui.png"
   ) {
     result.textContent = "Uduzdun";
   }
   if (
-    compImg.src ===
-      "https://files.modern.az/articles/2021/09/29/467399.jpg" &&
+    compImg.src === "https://files.modern.az/articles/2021/09/29/467399.jpg" &&
     userImg.src ===
-      "https://file.removal.ai/preview/b9a522da-2292-429c-b216-79668865911a-dwayne-the-rock-johnson-gettyimages-1061959920.png"
+      "https://www.slazzer.com/downloads/b997ce66-f516-11ee-9f1b-42010a80000a/%5Bremoval.ai%5D_b9a522da-2292-429c-b216-79668865911a-dwayne-the-rock-johnson-gettyimages-1061959920_prev_ui.png"
   ) {
     result.textContent = "Uddun";
   }
   if (
     compImg.src ===
-      "https://file.removal.ai/preview/a14a167d-1688-4040-b989-96a42927a718-20568308-rock-paper-scissors-paper-hand-isolated-on-white.png" &&
+      "https://www.slazzer.com/downloads/20e72134-f517-11ee-8c36-42010a80000a/1000_F_243068964_BqrpjOdMd2U7ZEGLjiO8l7h4Ut1j9hcA_prev_ui.png" &&
     userImg.src ===
-      "https://file.removal.ai/preview/b9a522da-2292-429c-b216-79668865911a-dwayne-the-rock-johnson-gettyimages-1061959920.png"
+      "https://www.slazzer.com/downloads/b997ce66-f516-11ee-9f1b-42010a80000a/%5Bremoval.ai%5D_b9a522da-2292-429c-b216-79668865911a-dwayne-the-rock-johnson-gettyimages-1061959920_prev_ui.png"
   ) {
     result.textContent = "Uduzdun";
   }
   if (
     compImg.src ===
-      "https://file.removal.ai/preview/a14a167d-1688-4040-b989-96a42927a718-20568308-rock-paper-scissors-paper-hand-isolated-on-white.png" &&
-    userImg.src ===
-      "https://files.modern.az/articles/2021/09/29/467399.jpg"
+      "https://www.slazzer.com/downloads/20e72134-f517-11ee-8c36-42010a80000a/1000_F_243068964_BqrpjOdMd2U7ZEGLjiO8l7h4Ut1j9hcA_prev_ui.png" &&
+    userImg.src === "https://files.modern.az/articles/2021/09/29/467399.jpg"
   ) {
     result.textContent = "Uddun";
   }
 }
-function createImgas() {
+function createImegas() {
   user_and_comp[0].appendChild(userImg);
   user_and_comp[1].appendChild(compImg);
   userImg.setAttribute("src", "...");
-} 
+}
